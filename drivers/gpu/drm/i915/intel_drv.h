6ea7950c8d2816336786447f78e5139c04311649
Ville Syrjälä <ville.syrjala@linux.intel.com>
2015-07-07 22:24:26 +0300

    drm/i915: Trick CL2 into life on CHV when using pipe B with port B
    Normmally the common lane in a PHY channel gets powered up when some
    of the data lanes get powered up. But when we're driving port B with
    pipe B we don't want to enabled any of the data lanes, and just want
    the DPLL in the common lane to be active.
    
    To make that happens we have to temporarily enable some data lanes
    after which we can access the DPLL registers in the common lane. Once
    the pipe is up and running we can drop the power override on the data
    lanes allowing them to shut down. From this point forward the common
    lane will in fact stay powered on until the data lanes in the other
    channel get powered down.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -782,6 +782,7 @@ struct intel_digital_port {
 	struct intel_dp dp;
 	struct intel_hdmi hdmi;
 	enum irqreturn (*hpd_pulse)(struct intel_digital_port *, bool);
+	bool release_cl2_override;
 };
 
 struct intel_dp_mst_encoder {
@@ -1372,6 +1373,8 @@ void intel_display_set_init_power(struct drm_i915_private *dev, bool enable);
 
 void chv_phy_powergate_lanes(struct intel_encoder *encoder,
 			     bool override, unsigned int mask);
+bool chv_phy_powergate_ch(struct drm_i915_private *dev_priv, enum dpio_phy phy,
+			  enum dpio_channel ch, bool override);
 
 
 /* intel_pm.c */
