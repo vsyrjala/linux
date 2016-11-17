e4d8ef1e013c2385d41081f6b6b99855776e2b11
Ville Syrjälä <ville.syrjala@linux.intel.com>
2016-11-17 21:12:07 +0200

    drm: Nuke fb->bits_per_pixel
    Replace uses of fb->bits_per_pixel with fb->format->cpp[0]*8.
    Less duplicated information is a good thing.
    
    Note that I didn't put parens around the cpp*8 in the below cocci script,
    on account of not wanting spurious parens all over the place. Instead I
    did the unsafe way, and tried to look over the entire diff to spot if
    any dangerous expressions were produced. I didn't see any.
    
    There are some cases where previously the code did X*bpp/8, so the
    division happened after the multiplication. Those are now just X*cpp
    so the division effectively happens before the multiplication,
    but that is perfectly fine since bpp is always a multiple of 8.
    
    @@
    struct drm_framebuffer *fb;
    expression E;
    @@
     drm_helper_mode_fill_fb_struct(...) {
    	...
    -	fb->bits_per_pixel = E;
    	...
     }
    
    @@
    struct vmw_framebuffer_surface *vfb;
    expression E;
    @@
    - vfb->base.base.bits_per_pixel = E;
    
    @@
    struct vmw_framebuffer_dmabuf *vfb;
    expression E;
    @@
    - vfb->base.base.bits_per_pixel = E;
    
    @@
    struct drm_framebuffer *fb;
    expression E;
    @@
     i9xx_get_initial_plane_config(...) {
    	...
    -	fb->bits_per_pixel = E;
    	...
     }
    
    @@
    struct drm_framebuffer *fb;
    expression E;
    @@
     ironlake_get_initial_plane_config(...) {
    	...
    -	fb->bits_per_pixel = E;
    	...
     }
    
    @@
    struct drm_framebuffer *fb;
    expression E;
    @@
     skylake_get_initial_plane_config(...) {
    	...
    -	fb->bits_per_pixel = E;
    	...
     }
    
    @@
    struct drm_framebuffer fb;
    expression E;
    @@
    (
    - E * fb.bits_per_pixel / 8
    + E * fb.format->cpp[0]
    |
    - fb.bits_per_pixel / 8
    + fb.format->cpp[0]
    |
    - E * fb.bits_per_pixel >> 3
    + E * fb.format->cpp[0]
    |
    - fb.bits_per_pixel >> 3
    + fb.format->cpp[0]
    |
    - (fb.bits_per_pixel + 7) / 8
    + fb.format->cpp[0]
    |
    - fb.bits_per_pixel
    + fb.format->cpp[0] * 8
    |
    - fb.format->cpp[0] * 8 != 8
    + fb.format->cpp[0] != 1
    )
    
    @@
    struct drm_framebuffer *fb;
    expression E;
    @@
    (
    - E * fb->bits_per_pixel / 8
    + E * fb->format->cpp[0]
    |
    - fb->bits_per_pixel / 8
    + fb->format->cpp[0]
    |
    - E * fb->bits_per_pixel >> 3
    + E * fb->format->cpp[0]
    |
    - fb->bits_per_pixel >> 3
    + fb->format->cpp[0]
    |
    - (fb->bits_per_pixel + 7) / 8
    + fb->format->cpp[0]
    |
    - fb->bits_per_pixel
    + fb->format->cpp[0] * 8
    |
    - fb->format->cpp[0] * 8 != 8
    + fb->format->cpp[0] != 1
    )
    
    @@
    @@
    - (8 * 8)
    + 8 * 8
    
    @@
    struct drm_framebuffer fb;
    @@
    - (fb.format->cpp[0])
    + fb.format->cpp[0]
    
    @@
    struct drm_framebuffer *fb;
    @@
    - (fb->format->cpp[0])
    + fb->format->cpp[0]
    
    @@
    @@
     struct drm_framebuffer {
    	 ...
    -	 int bits_per_pixel;
    	 ...
     };
    
    v2: Clean up the 'cpp*8 != 8' and '(8 * 8)' cases (Laurent)
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com> (v1)

--- drivers/gpu/drm/vmwgfx/vmwgfx_kms.c
+++ drivers/gpu/drm/vmwgfx/vmwgfx_kms.c
@@ -591,7 +591,6 @@ static int vmw_kms_new_framebuffer_surface(struct vmw_private *dev_priv,
 	/* XXX get the first 4 from the surface info */
 	vfbs->base.base.format = drm_format_info(pixel_format);
 	vfbs->base.base.pixel_format = pixel_format;
-	vfbs->base.base.bits_per_pixel = mode_cmd->bpp;
 	vfbs->base.base.pitches[0] = mode_cmd->pitch;
 	vfbs->base.base.width = mode_cmd->width;
 	vfbs->base.base.height = mode_cmd->height;
@@ -899,7 +898,6 @@ static int vmw_kms_new_framebuffer_dmabuf(struct vmw_private *dev_priv,
 	vfbd->base.base.dev = dev;
 	vfbd->base.base.format = drm_format_info(pixel_format);
 	vfbd->base.base.pixel_format = pixel_format;
-	vfbd->base.base.bits_per_pixel = mode_cmd->bpp;
 	vfbd->base.base.pitches[0] = mode_cmd->pitch;
 	vfbd->base.base.width = mode_cmd->width;
 	vfbd->base.base.height = mode_cmd->height;
