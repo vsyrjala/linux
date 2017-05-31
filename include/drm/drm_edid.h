ea8b6836091649cf7032663a9c2ddc6f13ba8905
Uma Shankar <uma.shankar@intel.com>
2017-05-31 15:40:56 +0530

    drm: Enable HDR infoframe support
    Enable Dynamic Range and Mastering Infoframe for HDR
    content, which is defined in CEA 861.3 spec.
    
     The metadata will be computed based on blending
    policy in userspace compositors and passed as a connector
    property blob to driver. The same will be sent as infoframe
    to panel which support HDR.
    
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>

--- include/drm/drm_edid.h
+++ include/drm/drm_edid.h
@@ -365,6 +365,10 @@ drm_hdmi_avi_infoframe_quant_range(struct hdmi_avi_infoframe *frame,
 				   bool rgb_quant_range_selectable,
 				   bool is_hdmi2_sink);
 
+int
+drm_hdmi_infoframe_set_hdr_metadata(struct hdmi_drm_infoframe *frame,
+				    void *hdr_source_metadata);
+
 /**
  * drm_eld_mnl - Get ELD monitor name length in bytes.
  * @eld: pointer to an eld memory structure with mnl set
