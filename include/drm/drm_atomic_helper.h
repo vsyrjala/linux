473d3942502acb10e412f681331597f77b43e9a4
Ville Syrjälä <ville.syrjala@linux.intel.com>
2017-06-27 20:13:06 +0300

    commited state dup

--- include/drm/drm_atomic_helper.h
+++ include/drm/drm_atomic_helper.h
@@ -179,6 +179,10 @@ drm_atomic_helper_connector_duplicate_state(struct drm_connector *connector,
 struct drm_atomic_state *
 drm_atomic_helper_duplicate_state(struct drm_device *dev,
 				  struct drm_modeset_acquire_ctx *ctx);
+struct drm_atomic_state *
+drm_atomic_helper_duplicate_commited_state(struct drm_device *dev);
+void
+drm_atomic_helper_clean_commited_state(struct drm_atomic_state *state);
 void
 __drm_atomic_helper_connector_destroy_state(struct drm_connector_state *state);
 void drm_atomic_helper_connector_destroy_state(struct drm_connector *connector,
