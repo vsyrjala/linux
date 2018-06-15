6972989c49a01426ac7036b458cbc0b27bb069d0
Ville Syrjälä <ville.syrjala@linux.intel.com>
2018-06-15 15:50:56 +0300

    drm/imx: Remove the bogus possible_clones setup
    dw_hdmi_imx_bind() does not call so the possible_clones mask being set
    here is entirely bogus, not to mention the bits set for non-existent
    encoders. Just clear possible_clones until someone fixes this properly.
    
    Cc: Philipp Zabel <p.zabel@pengutronix.de>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- drivers/gpu/drm/imx/imx-drm-core.c
+++ drivers/gpu/drm/imx/imx-drm-core.c
@@ -158,7 +158,7 @@ int imx_drm_encoder_parse_of(struct drm_device *drm,
 	encoder->possible_crtcs = crtc_mask;
 
 	/* FIXME: this is the mask of outputs which can clone this output. */
-	encoder->possible_clones = ~0;
+	encoder->possible_clones = 0;
 
 	return 0;
 }
