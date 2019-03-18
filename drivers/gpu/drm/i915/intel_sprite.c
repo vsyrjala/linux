e517d96731911cb685a715ad7b5d19b161c0857f
Ville Syrjälä <ville.syrjala@linux.intel.com>
2019-03-18 21:33:27 +0200

    drm/i915: Remove 64bpp formats from ICL SDR plane format lists
    ICL SDR planes do not support 64bpp formats. Remove them from
    the format lists.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- drivers/gpu/drm/i915/intel_sprite.c
+++ drivers/gpu/drm/i915/intel_sprite.c
@@ -2122,12 +2122,7 @@ static const u32 icl_sdr_uv_plane_formats[] = {
 	DRM_FORMAT_P010,
 	DRM_FORMAT_P012,
 	DRM_FORMAT_P016,
-	DRM_FORMAT_Y210,
-	DRM_FORMAT_Y212,
-	DRM_FORMAT_Y216,
 	DRM_FORMAT_XVYU2101010,
-	DRM_FORMAT_XVYU12_16161616,
-	DRM_FORMAT_XVYU16161616,
 };
 
 static const u32 icl_hdr_plane_formats[] = {
