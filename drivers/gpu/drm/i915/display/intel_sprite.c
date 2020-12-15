7c2232dfc9f71aea6a189711d9f24f4b08c4cdf1
Ville Syrjälä <ville.syrjala@linux.intel.com>
2020-12-15 21:03:51 +0200

    drm/i915: Fix vblank evasion with vrr
    With vrr enabled the hardware no longer latches the registers
    automagically at vblank start. The point at which it will do the
    latching even when no push has been sent is the vmax decision
    boundary. That is the thing we need to evade to avoid our
    register latching to get split between two frames.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- drivers/gpu/drm/i915/display/intel_sprite.c
+++ drivers/gpu/drm/i915/display/intel_sprite.c
@@ -100,19 +100,15 @@ void intel_pipe_update_start(const struct intel_crtc_state *new_crtc_state)
 	if (new_crtc_state->uapi.async_flip)
 		return;
 
-	vblank_start = intel_mode_vblank_start(adjusted_mode);
+	if (new_crtc_state->vrr.enable)
+		vblank_start = intel_vrr_vmax_vblank_start(new_crtc_state);
+	else
+		vblank_start = intel_mode_vblank_start(adjusted_mode);
 
 	/* FIXME needs to be calibrated sensibly */
 	min = vblank_start - intel_usecs_to_scanlines(adjusted_mode,
 						      VBLANK_EVASION_TIME_US);
 	max = vblank_start - 1;
-
-	/* In case of VRR, we stall the Push and updates if too close to Vmax */
-	if (new_crtc_state->vrr.enable) {
-		min = new_crtc_state->vrr.vmax - intel_usecs_to_scanlines(adjusted_mode,
-									  VBLANK_EVASION_TIME_US);
-		max = new_crtc_state->vrr.vmax - 1;
-	}
 
 	if (min <= 0 || max <= 0)
 		goto irq_disable;
