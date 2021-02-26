4d0e84abf770eac790af2242f4818b411ba3cf92
Ville Syrjälä <ville.syrjala@linux.intel.com>
2021-02-26 15:53:55 +0200

    drm/i915: Stuff SAGV watermark into a sub-structure
    We'll want a SAGV transition watermark as well. Prepare
    for that by collecting SAGV wm0 into a sub-strcture.
    
    Cc: Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -9387,7 +9387,7 @@ static void verify_wm_state(struct intel_crtc *crtc,
 			if (skl_wm_level_equals(&hw_plane_wm->wm[level],
 						&sw_plane_wm->wm[level]) ||
 			    (level == 0 && skl_wm_level_equals(&hw_plane_wm->wm[level],
-							       &sw_plane_wm->sagv_wm0)))
+							       &sw_plane_wm->sagv.wm0)))
 				continue;
 
 			drm_err(&dev_priv->drm,
@@ -9444,7 +9444,7 @@ static void verify_wm_state(struct intel_crtc *crtc,
 			if (skl_wm_level_equals(&hw_plane_wm->wm[level],
 						&sw_plane_wm->wm[level]) ||
 			    (level == 0 && skl_wm_level_equals(&hw_plane_wm->wm[level],
-							       &sw_plane_wm->sagv_wm0)))
+							       &sw_plane_wm->sagv.wm0)))
 				continue;
 
 			drm_err(&dev_priv->drm,
