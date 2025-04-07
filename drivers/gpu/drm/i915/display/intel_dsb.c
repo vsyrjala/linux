bc7611ac212dbbb05fa7ee3c2e94636c56ecaabd
Ville Syrjälä <ville.syrjala@linux.intel.com>
2025-04-07 19:00:53 +0300

    drm/i915/flipq: Provide the nuts and bolts code for flip queue
    Provide the lower level code for PIPEDMC based flip queue.
    
    We'll use the so called semi-full flip queue mode where the
    PIPEDMC will start the provided DSB on a scanline a little
    ahead of the vblank. We need to program the triggering scanline
    early enough so that the DSB has enough time to complete writing
    all the double buffered registers before they get latched (at
    start of vblank).
    
    The firmware implements several queues:
    - 3 "plane queues" which execute a single DSB per entry
    - 1 "general queue" which can apparently execute 2 DSBs per entry
    - 1 vestigial "fast queue" that replaced the "simple flip queue"
      on ADL+, but this isn't supposed to be used due to issues.
    
    But we only need a single plane queue really, and we won't actually
    use it as a real queue because we don't allow queueing multiple commits
    ahead of time. So the whole thing is perhaps useless. I suppose
    there migth be some power saving benefits if we would get the flip
    scheduled by userspace early and then could keep some hardware powered
    off a bit longer until the DMC kicks off the flipq programming. But that
    is pure speculation at this time and needs to be proven.
    
    The code to hook up the flip queue into the actual atomic commit
    path will follow later.
    
    TODO: need to think how to do the "wait for DMC firmware load" nicely
          need to think about VRR and PSR
          etc.
    
    v2: Don't write DMC_FQ_W2_PTS_CFG_SEL on pre-lnl
        Don't oops at flipq init if there is no dmc
    v3: Adapt to PTL+ flipq changes (different queue entry
        layout, different trigger event, need VRR TG)
        Use the actual CDCLK frequency
        Ask the DSB code how long things are expected to take
    v3: Adjust the cdclk rounding (docs are 100% vague, Windows
        rounds like this)
        Initialize some undocumented magic DMC variables on PTL
    v4: Use PIPEDMC_FQ_STATUS for busy check (the busy bit in
        PIPEDMC_FQ_CTRL is apparently gone on LNL+)
        Based the preempt timeout on the max exec time
        Preempt before disabling the flip queue
        Order the PIPEDMC_SCANLINECMP* writes a bit more carefully
        Fix some typos
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- drivers/gpu/drm/i915/display/intel_dsb.c
+++ drivers/gpu/drm/i915/display/intel_dsb.c
@@ -13,6 +13,7 @@
 #include "intel_de.h"
 #include "intel_display_rpm.h"
 #include "intel_display_types.h"
+#include "intel_dmc_regs.h"
 #include "intel_dsb.h"
 #include "intel_dsb_buffer.h"
 #include "intel_dsb_regs.h"
